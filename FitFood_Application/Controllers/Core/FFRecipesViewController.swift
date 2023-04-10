e9760b592a3634564654aa773ef74b42a53884b5
Grachev Aleksandr <aleksandr.grachev@openmedia.co>
2023-04-10 17:02:46 +0300

    #comment Add proxyman, Add recipes collection view

--- FitFood_Application/Controllers/Core/FFRecipesViewController.swift
+++ FitFood_Application/Controllers/Core/FFRecipesViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 final class FFRecipesViewController: FFBaseViewController {
+    // MARK: - Properties
+    
+    private var sections = ["Popular menus", "Сategories", "Recently"]
+    
+    // MARK: - Subviews
+    
+    private lazy var collectionView = UICollectionView(frame: .zero,
+                                                       collectionViewLayout: UICollectionViewFlowLayout())
     
     // MARK: - LifeCycle
     
@@ -16,12 +24,59 @@ final class FFRecipesViewController: FFBaseViewController {
         
         configureViews()
     }
-    
-    // MARK: - Layout
-    
-    private func configureViews() {
+}
+
+// MARK: - Layout
+
+private extension FFRecipesViewController {
+    func configureViews() {
         title = Asset.Strings.recipes
-        navigationController?.navigationBar.addBottomBorder(with: Asset.Colors.separator,
-                                                            height: 0.5)
+        navigationController?.navigationBar.addBottomBorder(
+            with: Asset.Colors.separator,
+            height: 0.5
+        )
+        configureCollectionView()
     }
+    
+    func configureCollectionView() {
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.backgroundColor = Asset.Colors.background
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints {
+            $0.left.equalToSuperview()
+            $0.right.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension FFRecipesViewController: UICollectionViewDelegate {
+    
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension FFRecipesViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension FFRecipesViewController: UICollectionViewDelegateFlowLayout {
+    
 }
--- FitFood_Application/Resources/AppDelegate.swift
+++ FitFood_Application/Resources/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseCore
+import Atlantis
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        Atlantis.start(hostName: "AleksandrGrachevs-MacBook-Air.local.")
         FirebaseApp.configure()
         return true
     }
